93b89922a2b043d3b01bda09a8d061d307b03d21
Abel Godoy <abelgodoy.1802@gmail.com>
2026-04-07 14:03:33 -0300

    minor fixes

---
+++ src/main/java/com/automationexercise/steps/HomeSteps.java
@@ -0,0 +1,9 @@
+package com.automationexercise.steps;
+
+import io.cucumber.java.en.Given;
+
+public class HomeSteps {
+    @Given("The user launches the browser")
+    public void theUserLaunchesTheBrowser() {
+    }
+}
