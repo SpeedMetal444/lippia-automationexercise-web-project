404560a1842885f31a3022870dcf5218c395f534
Abel Godoy <abelgodoy.1802@gmail.com>
2026-04-13 18:54:22 -0300

    Testcases.feature finished

--- src/test/java/com/automationexercise/steps/navigation/TestCasesSteps.java
+++ src/test/java/com/automationexercise/steps/navigation/TestCasesSteps.java
@@ -1,5 +1,6 @@
 package com.automationexercise.steps.navigation;
 
+import com.automationexercise.services.common.CommonService;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 
@@ -7,6 +8,6 @@ public class TestCasesSteps {
 
     @Then("^The user verifies that user is navigated to test cases page successfully$")
     public void theUserVerifiesThatUserIsNavigatedToTestCasesPageSuccessfully() {
-        throw new PendingException();
+        CommonService.verifyTextVisibility("Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:");
     }
 }
--- src/test/resources/web/features/navigation/testcases.feature
+++ src/test/resources/web/features/navigation/testcases.feature
@@ -5,7 +5,7 @@ Feature: Test Cases - Verify Test Cases Page
     Given The user navigates to url "http://automationexercise.com"
     And The user verifies that home page is visible successfully
 
-  @VerifyTestCasesPage @Candidate
+  @VerifyTestCasesPage @Automated
   Scenario: User can navigate to Test Cases page
     When The user clicks on "Test Cases" button
     Then The user verifies that user is navigated to test cases page successfully
